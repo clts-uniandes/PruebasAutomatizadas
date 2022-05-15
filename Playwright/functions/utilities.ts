ed27ee43a9c7b25dd2e9506e099d4eb6a4506df0
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2022-05-14 23:08:26 -0500

    ajustes utilities

--- Playwright/functions/utilities.ts
+++ Playwright/functions/utilities.ts
@@ -2,16 +2,16 @@ import Env from "../util/environment";
 
 export default class Utilities {
 
-    public screenshotPath: string;
+    public scenarioPath: string;
 
-    constructor() {
-        this.screenshotPath = "";
+    constructor(scenarioPath: string) {
+        this.scenarioPath = scenarioPath;
     }
 
-    public generateScreenshotPath(testScenarioName: string, screenshotId:number) : string {
+    public generateScreenshotPath(screenshotId:number) : string {
         //const postsLink = this.page.$("section.post-full-tags");
-        if(testScenarioName != null && screenshotId != null) {
-            return Env.SCREENSHOT_FOLDER + testScenarioName + "/" + screenshotId+".png";
+        if(screenshotId != null) {
+            return Env.SCREENSHOT_FOLDER + this.scenarioPath + "/" + screenshotId+".png";
         } else {
             throw new Error("No valid parameters");
         }
