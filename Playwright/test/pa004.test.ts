4400fca88082156fa89b81ae363a884fcefee96c
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2022-05-15 19:39:51 -0500

    codigo pruebas ghost3 con screenshots

--- Playwright/test/pa004.test.ts
+++ Playwright/test/pa004.test.ts
@@ -6,12 +6,16 @@ import PageEditorPage from "../page-object/page-editor.page";
 import Env from "../util/environment";
 
 import { test, expect } from '@playwright/test';
+import Utilities from "../functions/utilities";
 
-test.describe("PA004 - ", () => {
+let screenshotNumber = 1;
+
+test.describe("PA004 - Creación de página con título que trae carácteres no permitidos", () => {
 
     let browser: Browser;
     let context: BrowserContext;
     let page: Page;
+    let utilities: Utilities;
 
     //My pageObjects
     let login: LoginPage;
@@ -25,6 +29,7 @@ test.describe("PA004 - ", () => {
         });
         context = await browser.newContext({ viewport: { width: 1200, height: 600 } });
         page = await context.newPage();
+        utilities = new Utilities("PA004");
 
         //TODO GIVEN url tol login
         await page.goto(Env.BASE_URL + Env.ADMIN_SECTION);
@@ -34,25 +39,33 @@ test.describe("PA004 - ", () => {
         pageEditor = new PageEditorPage(page);
     });
 
-    test("should create and edit a page - positive scenario", async () => {
+    test("should create and edit a page with unallowed chars - positive scenario", async () => {
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         //TODO WHEN I log in
         await login.signInWith(Env.USER, Env.PASS);
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         //TODO WHEN I navigate to Page module
         await home.clickPagesLink();
         //TODO THEN I expected that url will updated
         expect(page.url()).toContain("/#/pages");
-
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageGhost.clickNewPageLink();
         expect(page.url()).toContain("/#/editor/page");
-
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.fillPageTitle("Titulo de pagina con caracteres especiales @$@@$^&%$@#$@# utilizando playwright");
         expect(await pageEditor.findErrorMessage()).toBeNull();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.fillPostContent("Contenido de pagina con caracteres especiales utilizando playwright");
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.clickPublishLink();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.clickPublishButton();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.clickPagesLink();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         const linkCreatedPage = await pageGhost.findPageByTitle("Titulo de pagina con caracteres especiales @$@@$^&%$@#$@# utilizando playwright");
         expect(linkCreatedPage).not.toBeNull();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
     });
 
     test.afterAll(async () => {
--- Playwright/test/pa006.test.ts
+++ Playwright/test/pa006.test.ts
@@ -9,14 +9,16 @@ import Util from "../util/util";
 
 import { test, expect } from '@playwright/test';
 import SitePage from "../page-object/site.page";
+import Utilities from "../functions/utilities";
 
-let screenshotNumber = 0;
+let screenshotNumber = 1;
 
-test.describe("PA006 - ", () => {
+test.describe("PA006 - Renombrar página y componentes navbar asociados", () => {
 
     let browser: Browser;
     let context: BrowserContext;
     let page: Page;
+    let utilities: Utilities;
 
     //My pageObjects
     let login: LoginPage;
@@ -32,6 +34,7 @@ test.describe("PA006 - ", () => {
         });
         context = await browser.newContext({ viewport: { width: 1200, height: 600 } });
         page = await context.newPage();
+        utilities = new Utilities("PA006");
 
         //TODO GIVEN url tol login
         await page.goto(Env.BASE_URL + Env.ADMIN_SECTION);
@@ -44,77 +47,106 @@ test.describe("PA006 - ", () => {
         site = new SitePage(page);
     });
 
-    test("should create a page, assign page to navbar, verify navbar work in home page, edit page title, edit navbar link and finally verify if home page show nav bar correctly - positive scenario", async () => {        //TODO WHEN I log in
+    test("should edit page title, edit navbar link and verify if home page show nav bar correctly - positive scenario", async () => {
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
+        //TODO WHEN I log in
         await login.signInWith(Env.USER, Env.PASS);
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         //TODO WHEN I navigate to Page module
         await home.clickPagesLink();
         //TODO THEN I expected that url will updated
         expect(page.url()).toContain("/#/pages");
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
 
         //TODO WHEN I navigate to Page Editor
         await pageGhost.clickNewPageLink();
         expect(page.url()).toContain("/#/editor/page");
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
 
         //TODO WHEN I create a page
         await pageEditor.fillPageTitle("title-pa006");
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.fillPostContent("Contenido de pagina utilizando playwright");
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.clickPublishLink();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.clickPublishButton();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.clickPagesLink();
         //TODO THEN I expected is there the page in general list
         const linkCreatedPage = await pageGhost.findPageByTitle("title-pa006");
         expect(linkCreatedPage).not.toBeNull();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
 
         //TODO WHEN I navigate to design page
         await home.clickDesignLink();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
 
         //TODO WHEN I crate a navbar
         await design.fillLabelForNewNav("NAV PA006");
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await design.fillUrlForNewNav("title-pa006");
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await design.clickAddNewNavButton();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await design.clickSaveButton();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
 
         //TODO WHEN I navigate to home page in order to see the navbar work
         await home.clickViewSiteLink();
         const linkNavCreated = await site.findNavByTitle("NAV PA006");
         expect(linkNavCreated).not.toBeNull();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await site.navigateToNavLink(linkNavCreated);
         expect(await site.isThereDescriptionWith("Contenido de pagina utilizando playwright")).toBe(true);
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
 
         //TODO WHEN I navigate to Page module
         await home.clickPagesLink();
         //TODO THEN I expected that url will updated
         expect(page.url()).toContain("/#/pages");
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
 
         //TODO THEN I expected is there the page in general list
         const linkCreatedPageSearch2 = await pageGhost.findPageByTitle("title-pa006");
         expect(linkCreatedPageSearch2).not.toBeNull();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         //TODO WHEN I edited the created page before
         await pageGhost.navigateToEditionLink(linkCreatedPageSearch2);
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.fillPageTitle("title-pa006-edited");
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         //TODO EDIT page link
         await pageEditor.updatePageURLWith("title-pa006-edited");
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.clickUpdateLink();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.clickUpdateButton();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await pageEditor.clickPagesLink();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         //TODO THEN I expected is there the edited page in general list
         const linkEditedPage = await pageGhost.findPageByTitle("title-pa006-edited");
         expect(linkEditedPage).not.toBeNull();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
 
         //TODO WHEN I navigate to design page
         await home.clickDesignLink();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
 
         //TODO WHEN I edit the navbar created
         await design.updateNavBar("NAV PA006", "NAV PA006 EDITED",Env.BASE_URL+"/title-pa006-edited");
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
 
         //TODO WHEN I navigate to home page in order to see if navbar works
         await home.clickViewSiteLink();
         const linkNavEdited = await site.findNavByTitle("NAV PA006 EDITED");
         expect(linkNavEdited).not.toBeNull();
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
         await site.navigateToNavLink(linkNavEdited);
-
         //TODO THEN I navigate to home page in order to see the navbar work
         expect(await site.isThereDescriptionWith("Contenido de pagina utilizando playwright")).toBe(true);
+        await page.screenshot({path: utilities.generateScreenshotPath(screenshotNumber++)});
     });
 
     test.afterAll(async () => {
