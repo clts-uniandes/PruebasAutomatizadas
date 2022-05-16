d1f24544e93b188f81f54a18d5befef1e6ea0509
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2022-05-15 21:50:39 -0500

    limpieza raiz de repo para entrega

--- Cypress/pruebas_e2e/cypress/integration/PruebasE2E/pageObjects/ContentMainPage.js
+++
@@ -1,9 +0,0 @@
-class ContentMainPage {
-
-    getNavPageLink(pageLink, pageName) {
-        return cy.get(`a[href*="http://localhost:2368/${pageLink}/"]`).contains(pageName)
-    }
-    
-}
-
-export default ContentMainPage;
